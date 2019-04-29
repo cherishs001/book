4631c6ec04ff77786f69a51aa5ddf7d291b430c3
TenmaHiltonWhat <expWindows@Outlook.com>
2019-04-29 20:48:15 +0800

    Cookie Operation

---
+++ src/web/Cookie.ts
@@ -0,0 +1,21 @@
+export function SetCookie(n,v,e)
+{
+  const exdate=new Date();
+  exdate.setDate(exdate.getDate()+e);
+  document.cookie=n+ "=" +escape(v)+((e==null) ? "" : ";expires="+exdate.toGMTString());
+}
+export function GetCookie(n)
+{
+  if (document.cookie.length>0)
+  {
+    s=document.cookie.indexOf(n + "=");
+    if (s!=-1)
+    { 
+      s=s + n.length+1;
+      e=document.cookie.indexOf(";",s);
+      if (e==-1) e=document.cookie.length;
+      return unescape(document.cookie.substring(s,e));
+    } 
+  }
+  return null
+}
