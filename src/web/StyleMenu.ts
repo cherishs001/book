9860bdd4ed2c73a0cff26e35057401805a0e7ec4
SCLeo <scucumber@outlook.com>
2019-05-03 14:33:35 -0400

    Seperate scrollbar related css

--- src/web/StyleMenu.ts
+++ src/web/StyleMenu.ts
@@ -40,7 +40,7 @@ class Style {
     sheet.insertRule(`.rect.reading>.content a:active { color: ${this.def.linkActiveColor}; }`);
     sheet.insertRule(`.rect.reading>.content>.earlyAccess.block { background-color: ${this.def.contentBlockEarlyAccessColor}; }`);
     sheet.insertRule(`.rect>.comments>div { background-color: ${this.def.commentColor}; }`);
-    sheet.insertRule(`::-webkit-scrollbar-thumb { width: 10px; height: 10px; background-color: ${this.def.paperBgColor}; border-radius: 5px; }`);
+    sheet.insertRule(`::-webkit-scrollbar-thumb { background-color: ${this.def.paperBgColor}; }`);
     sheet.insertRule(`::-webkit-scrollbar-thumb:hover { background-color: ${this.def.linkColor}; }`);
     sheet.insertRule(`::-webkit-scrollbar-thumb:active { background-color: ${this.def.linkActiveColor}; }`);
 
